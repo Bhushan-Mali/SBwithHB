ce028e6db5930a171a90e8694e90cdbd9773cb6c
Bhushan-Mali <malib475@gmail.com>
2023-06-23 11:52:48 +0530

    second commit

--- src/main/java/com/example/dao/StaffDao.java
+++ src/main/java/com/example/dao/StaffDao.java
@@ -7,12 +7,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
-import org.hibernate.query.NativeQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-
 import com.example.entity.Staff;
 
 @Repository
